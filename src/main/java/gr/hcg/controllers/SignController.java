139ac6ea7e7ede730504dfef1604709cffec9579
Serafeim Papastefanos <spapas@gmail.com>
2022-02-10 09:24:04 +0200

    Improve

--- src/main/java/gr/hcg/controllers/SignController.java
+++ src/main/java/gr/hcg/controllers/SignController.java
@@ -5,6 +5,7 @@ import gr.hcg.check.PDFSignatureInfo;
 import gr.hcg.check.PDFSignatureInfoParser;
 import gr.hcg.services.UploadDocumentService;
 import gr.hcg.sign.Signer;
+import gr.hcg.views.JsonView;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Value;
@@ -57,7 +58,8 @@ public class SignController {
 
     }
 
-    private ResponseEntity<byte[]> handleUpload(String year, String folder, String protocol, String uuid, byte[] bytes) throws IOException {
+    //private ResponseEntity<byte[]> handleUpload(String year, String folder, String protocol, String uuid, byte[] bytes) throws IOException {
+    private JsonObject handleUpload(String year, String folder, String protocol, String uuid, byte[] bytes) throws IOException {
         String path = uploadDocumentService.handleUpload(year, folder, protocol, uuid, bytes);
 
         final HttpHeaders headers = new HttpHeaders();
@@ -65,8 +67,18 @@ public class SignController {
         JsonObject jo = new JsonObject();
         jo.addProperty("uuid", uuid);
         jo.addProperty("path", path);
+        return jo;
 
-        return new ResponseEntity<>(jo.toString().getBytes(StandardCharsets.UTF_8), headers, HttpStatus.OK);
+
+        //return new ResponseEntity<>(jo.toString().getBytes(StandardCharsets.UTF_8), headers, HttpStatus.OK);
+    }
+
+    private Object respondHtmlOrJson(Optional<Boolean> json, Model model, HttpServletResponse response) {
+        if (json.isPresent()) {
+            return JsonView.Render(model, response );
+        } else {
+            return "sign";
+        }
     }
 
     @PostMapping("/sign")
@@ -77,6 +89,7 @@ public class SignController {
                                    @RequestParam(value = "folder") String folder,
                                    @RequestParam(value = "apikey") String apikey,
                                    @RequestParam(value = "signed") Optional<Boolean> signed,
+                                   @RequestParam(value = "json") Optional<Boolean> json,
                                    @RequestParam(value = "signName") Optional<String> signName,
                                    @RequestParam(value = "signReason") Optional<String> signReason,
                                    @RequestParam(value = "signLocation") Optional<String> signLocation,
@@ -86,62 +99,73 @@ public class SignController {
                                    // @RequestParam(value = "qrcode") Optional<String> qrcode,
                                    HttpServletResponse response ) {
 
+        model.addAttribute("uuid", null);
+        model.addAttribute("path", null);
         if (file.isEmpty()) {
             model.addAttribute("message", "Empty file");
-            return "sign";
+            model.addAttribute("error", true);
+            return respondHtmlOrJson(json, model, response);
         }
 
         if(!apikey.equals(signerapikey)) {
             model.addAttribute("message", "Wrong api key");
-            return "sign";
+            model.addAttribute("error", true);
+            return respondHtmlOrJson(json, model, response);
         }
         if(year==null || protocol == null || folder == null || year.equals("") || protocol.equals("") || folder.equals("")) {
             model.addAttribute("message", "Fill year, protocol and folder");
-            return "sign";
+            model.addAttribute("error", true);
+            return respondHtmlOrJson(json, model, response);
         }
 
         final String uuid = UUID.randomUUID().toString();
 
         if (signed.orElse(false) == true) {
-
-            byte[] bytes = new byte[0];
+            // Handle signed file
+            byte[] bytes;
             try {
                 bytes = file.getBytes();
                 List<PDFSignatureInfo> info = PDFSignatureInfoParser.getPDFSignatureInfo(bytes);
                 if (info.isEmpty()) {
-                    model.addAttribute("message", "Cannot validate siganture");
+                    model.addAttribute("error", true);
+                    model.addAttribute("message", "Cannot find siganture");
                 } else {
-                    return handleUpload(year, folder, protocol, uuid, bytes);
+                    JsonObject jo = handleUpload(year, folder, protocol, uuid, bytes);
+                    model.addAttribute("path", jo.get("path"));
+                    model.addAttribute("uuid", jo.get("uuid"));
+                    return respondHtmlOrJson(json, model, response);
                 }
 
             } catch (IOException | InvalidNameException | CertificateException | NoSuchAlgorithmException | SignatureException | InvalidKeyException | NoSuchProviderException e) {
 
                 e.printStackTrace();
-                model.addAttribute("message", "Cannot validate siganture");
-                return "sign";
+                model.addAttribute("error", true);
+                model.addAttribute("message", "Cannot validate siganture: " + e.getMessage());
+                return respondHtmlOrJson(json, model, response);
             }
+            return respondHtmlOrJson(json, model, response);
 
-
-            return "sign";
         } else {
-
+            // Handle non-signed file
             try {
                 ByteArrayOutputStream bos = new ByteArrayOutputStream();
                 String qrcode = docsUrlPrefix + uuid;
                 signer.sign(file.getInputStream(), bos, signName.orElse(null), signLocation.orElse(null), signReason.orElse(null), visibleLine1.orElse(null), visibleLine2.orElse(null), uuid, qrcode);
-                return handleUpload(year, folder, protocol, uuid, bos.toByteArray());
+                JsonObject jo = handleUpload(year, folder, protocol, uuid, bos.toByteArray());
+                model.addAttribute("path", jo.get("path"));
+                model.addAttribute("uuid", jo.get("uuid"));
+                return respondHtmlOrJson(json, model, response);
 
-            } catch (IOException | KeyStoreException | CertificateException | NoSuchAlgorithmException | UnrecoverableKeyException e) {
-                model.addAttribute("message", "General error!");
+            } catch (IOException | KeyStoreException | CertificateException | NoSuchAlgorithmException | UnrecoverableKeyException | IllegalStateException e) {
+                model.addAttribute("error", true);
+                model.addAttribute("message", "Error: " + e.getMessage());
                 e.printStackTrace();
-                return "sign";
+                //return "sign";
+                return respondHtmlOrJson(json, model, response);
             }
 
         }
 
-        //return new ModelAndView("sign", model.asMap());
-
-        //return "home";
     }
 
 
